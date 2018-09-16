e709df91c40315b01fb6591e5e6fb75e6f572d3b
Chris Temple <temple3188@gmail.com>
2018-09-16 18:43:28 +0100

    add global components

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,13 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
+import {
+  Styles,
+  Header,
+  Navigation,
+  Footer,
+  Copyright,
+} from '@noths/global-components'
 import './layout.css'
 
 const Layout = ({ children }) => (
@@ -28,17 +34,13 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+
+        <Styles />
+        <Header />
+        <Navigation />
+        {children}
+        <Footer />
+        <Copyright />
       </>
     )}
   />
